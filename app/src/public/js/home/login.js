2ceb7fa53249bbc08bd7fd21cf6698ea268a1c78
Hwang Ji Hwan <ghkdrbqhd2@naver.com>
2021-02-03 20:03:44 +0900

    Dom을 이용하여 HTML 객제 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,16 @@
 "use strict";
+//Dom -> Documenty Object Model 자바스크립트에서 html의 자료를 가져와서 제어할 수 있게 한다.
 
-console.log("Hello");
-console.log("bye")
+const id = document.querySelector("#id"),
+    password = document.querySelector("#password"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req ={
+        id: id.value,
+        password: password.value,
+    };
+    console.log(req);
+}
