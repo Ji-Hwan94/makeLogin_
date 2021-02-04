69df37884796851b727bc5e09bc5c33e8ac50b73
Hwang Ji Hwan <ghkdrbqhd2@naver.com>
2021-02-05 00:58:48 +0900

    회원가입 페이지 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,10 +2,10 @@
 //Dom -> Documenty Object Model 자바스크립트에서 html의 자료를 가져와서 제어할 수 있게 한다.
 
 const id = document.querySelector("#id"),
-    password = document.querySelector("#password"),
-    loginBtn = document.querySelector("button");
+    password = document.querySelector("#password");
+    //loginBtn = document.querySelector("button");
 
-loginBtn.addEventListener("click", login);
+//loginBtn.addEventListener("click", login);
 
 function login(){
     const req ={
