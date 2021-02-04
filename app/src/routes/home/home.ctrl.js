c6387bb7c6d900226bbd505cd3eec4f8e61b1e76
Hwang Ji Hwan <ghkdrbqhd2@naver.com>
2021-02-04 15:20:57 +0900

    백엔드에서 로그인 기능 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,4 +1,10 @@
 "use strict"
+
+const users = {
+    id: ['jihwan', 'kyubong', 'ghkdrbqhd'],
+    password: ['1234', '1234', 'qhd85246'],
+};
+
 const output = {
     hello:(req, res) => {
         res.render("home/index");
@@ -10,7 +16,22 @@ const output = {
 
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+       const id = req.body.id,
+       password = req.body.password;
+
+       if (users.id.includes(id)){
+           const idx = users.id.indexOf(id);
+           if (users.password[idx] === password){
+               return res.json({
+                   success: true,
+               });
+           }
+       }
+
+       return res.json({
+           success: false,
+           msg: "로그인에 실패되었습니다.",
+       })
     },
 }
 
