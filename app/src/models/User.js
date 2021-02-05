b463b649cddb27e2e4b4da4dd27405dfb3a01cda
Hwang Ji Hwan <ghkdrbqhd2@naver.com>
2021-02-05 17:14:55 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -8,16 +8,22 @@ class User {
     }
 
     login(){
-        const body = this.body;
+        const client = this.body;
         const { id, password } = UserStorage.getUserInfo(body.id);
         
         if (id) {
-            if (id === body.id && password === body.password){
+            if (id === client.id && password === client.password){
                 return { succes: true };
             }
             return { succes: false, msg: "비밀번호가 틀립니다."};
         }
         return { succes: false, msg: "존재하지 않는 아이디입니다."};
+    }
+
+    register(){
+        const client = this.body;
+        const responce = UserStorage.save(client);
+        return responce;       
     }
 }
         
